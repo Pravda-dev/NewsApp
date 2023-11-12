41a58a72dbc1c39f07dd0255281e20d865bf4a43
Konstantin <pravda11147@gmail.com>
2023-11-12 23:42:19 +0100

    UITabBarController

---
+++ NewsApp/NewsApp/Business/BusinessViewController.swift
@@ -0,0 +1,27 @@
+//
+//  BusinessViewController.swift
+//  NewsApp
+//
+//  Created by pravda on 12.11.2023.
+//
+
+import UIKit
+
+final class BusinessViewController: UIViewController {
+    //MARK: - GUI Variables
+    
+    //MARK: - Properties
+    
+    //MARK: - Life cycles
+  
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .green
+    }
+    //MARK: - Methods
+
+    //MARK: - Provate methods
+    
+    
+}
---
+++ NewsApp/NewsApp/Technology/TechnologyViewController.swift
@@ -0,0 +1,26 @@
+//
+//  TechnologyViewController.swift
+//  NewsApp
+//
+//  Created by pravda on 12.11.2023.
+//
+
+import UIKit
+
+final class TechnologyViewController: UIViewController {
+    //MARK: - GUI Variables
+    
+    //MARK: - Properties
+    
+    //MARK: - Life cycles
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+    
+    //MARK: - Methods
+
+    //MARK: - Provate methods
+}
